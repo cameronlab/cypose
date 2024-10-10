ba86f6494f42f57503208b74ac9cd22edacff2c1
Anton Avramov <31373450+mech314@users.noreply.github.com>
2024-10-10 13:57:23 -0600

    Updated models and cell counter script
    Better Anabaena model, 7002 classifier model and cell counter for tif files.

---
+++ cell_counter_v2.py
@@ -0,0 +1,54 @@
+import os
+import numpy as np
+import imageio
+import time
+from pathlib import Path
+from scipy.ndimage import label
+from concurrent.futures import ProcessPoolExecutor
+
+
+class CellCounter:
+    def __init__(self, directory=None):
+        """Set the directory where images are"""
+        self.path = Path(directory) if directory else Path(__file__).parent
+
+    def process_tif_file(self, filename):
+        """Open and process tif files."""
+        tif_file = imageio.mimread(filename)
+
+        # Convert to a numpy array
+        frame_array = np.array(tif_file)
+
+        # Label connected components
+        labeled_array, num_features = label(frame_array)
+
+        # Return the number of connected components
+        return num_features
+
+    def count_cells(self):
+        """Use multiprocessing to count the cells in the tif files."""
+        # Get the list of tif files
+        tif_files = list(self.path.glob('*labeled.tif'))
+
+        # Time it
+        start_time = time.time()
+
+        # Run in paralelle
+        with ProcessPoolExecutor() as executor:
+            # Map the function to each file
+            results = executor.map(self.process_tif_file, tif_files)
+
+            # Sum the cell counts from all files
+            total_cell_count = sum(results)
+            end_time = time.time()
+
+            elapsed_time = end_time - start_time
+            print(f'It took {elapsed_time:.2f} seconds to calculate the cell count')
+            print(f'Total cell count is {total_cell_count}')
+
+        return total_cell_count
+
+
+if __name__ == "__main__":
+    counter = CellCounter()
+    counter.count_cells()
